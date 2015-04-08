7bf63f696a47b40e336988c86747fdbf7c573bfe
James Churchill <james@smashdev.com>
2015-04-08 08:54:05 -0700

    Made a copy of the demo app code base

---
+++ demo-app-backup/js/models/project-task.js
@@ -0,0 +1,8 @@
+
+var ProjectTask = function () {
+    var self = this;
+
+    self.name = '';
+    self.completed = false;
+    self.hours = 0;
+};
---
+++ demo-app-backup/js/models/project.js
@@ -0,0 +1,9 @@
+
+var Project = function () {
+    var self = this;
+
+    self.projectId = 0;
+    self.name = '';
+    self.description = '';
+    self.tasks = [];
+};
