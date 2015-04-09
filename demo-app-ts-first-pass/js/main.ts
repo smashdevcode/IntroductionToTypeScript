b841f777e51debaabeefd0868b8fe01068c1e578
James Churchill <james@smashdev.com>
2015-04-09 14:33:46 -0700

    Removed first pass at the TS demo app

--- demo-app-ts-first-pass/js/main.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="../typings/knockout/knockout.d.ts" />
-/// <reference path="projects-view-model.ts" />
-
-var viewModel = new ProjectsViewModel();
-ko.applyBindings(viewModel);
--- demo-app-ts-first-pass/js/models/project.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="project-task.ts" />
-
-class Project {
-    constructor(public projectId: number = 0,
-                public name: string = '',
-                public description: string = '',
-                public tasks: ProjectTask[] = []) {
-    }
-}
